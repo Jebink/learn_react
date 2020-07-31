26a099ffa0e2a22fdaccdfe08819cc021420366a
JebinK <jebink7@gmail.com>
2020-07-31 21:41:59 +0530

    increment button

--- src/components/class_comp.js
+++ src/components/class_comp.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React , {Component} from "react";
 
 
-class HelloComp extends React.Component {
+class HelloComp extends Component {
+    constructor(){
+        super();
+        this.state = {
+            counter:0
+        }
+    }
+    increment = () => {
+        console.log("btn clicked");
+        this.setState({counter: this.state.counter + 1})
+    }
     render(){
         return (
-            <h1>Hello new Component</h1>
+            <div className="justify-content-center">
+            <button onClick = {this.increment} className="btn btn-primary">Increment</button>
+        <h1>{this.state.counter}</h1>
+            </div>
         )
     }
 }
