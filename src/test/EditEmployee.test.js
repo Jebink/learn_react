f47a51ea0cc411f86eecfeb8a80000669ad327f0
JebinK <jebink7@gmail.com>
2020-08-02 21:32:24 +0530

    simple jest test case added

---
+++ src/test/EditEmployee.test.js
@@ -0,0 +1,6 @@
+import { validateAge, validateName} from "../components/EditEmployee";
+describe("validate name", ()=>{
+    test("age", ()=>{
+        expect(validateName("JebinK")).toBeDefind()
+    })  
+})
