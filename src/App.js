dc69f02f5c994edf585fd524e44a7f0651f979e8
JebinK <jebink7@gmail.com>
2020-08-02 18:54:07 +0530

    routing demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import React from 'react';
-const Home = () => <h2> Home </h2>;
-const NewsFeed = () => <h2> News Feed </h2>;
-class App extends React.Component {
-    render() {
-        return (
-          <Router>
-            <React.Fragment>
-                <nav>
-                    <Link to={'/home'}> Home </Link><br />
-                    <Link to={'/news'}> News feed </Link><br />
-                    <Link to={'/contact'}> Contact </Link><br />
-                </nav>
-                <div>
-                    <Route exact path="/home" component={Home}/>
-                    <Route path="/news" component={NewsFeed}/>
-                    <Route path="/contact" render={() => <h3>Contact Us</h3>}/>
-                </div>
-            </React.Fragment>
-          </Router>   
-        );
-    }
+import React, { Component } from 'react';
+import './App.css';
+import { Route, Redirect, Switch } from 'react-router-dom'
+import EditEmployee from './components/EditEmployee';
+import Card from './components/Card';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" render={() => (<Redirect to="/card" />)} />
+          <Route path="/card" component={Card} />    
+          <Route path="/edit/:empId" component={EditEmployee} />
+        </Switch>
+      </div>
+    );
+  }
 }
 export default App;
